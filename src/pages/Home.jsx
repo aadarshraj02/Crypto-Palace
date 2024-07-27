2b889e1567a645371c46e6a324f6b67d6874dc50
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-27 11:56:58 +0530

    added a h1 and a p tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
-
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="hro">
+        <h1>
+          Largest <br /> Crypto Marketplace
+        </h1>
+        <p>
+          Welcome to the world largest cryptocurrency marketplace. Sign up to
+          explore more about cryptos.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
