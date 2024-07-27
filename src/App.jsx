bf9101ebec13535a96b6cf4a3da56710136bcbf7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-27 11:47:55 +0530

    added route to home.jsx and coin.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Coin from "./pages/Coin";
 
 function App() {
   return (
     <div className="min-h-screen text-white bg-custom-gradient">
       <Navbar />
+      <Routes>
+        <Route path="/" element={Home}></Route>
+        <Route path="/coin/:coinId" element={Coin}></Route>
+      </Routes>
     </div>
   );
 }
