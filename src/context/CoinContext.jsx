9c127f9057d9ee697dc936e1316632f402b41522
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-28 09:37:01 +0530

    added react context

--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -1,11 +1,15 @@
 import { createContext } from "react";
 
-export const CoinContext = createContext;
+export const CoinContext = createContext();
 
-const CoinContextProvider = (props)=>{
-    return (
-        <CoinContextProvider value={}>
-            {props.children}
-        </CoinContextProvider>
-    )
-}
+const CoinContextProvider = (props) => {
+  const contextValue = {};
+
+  return (
+    <CoinContext.Provider value={contextValue}>
+      {props.children}
+    </CoinContext.Provider>
+  );
+};
+
+export default CoinContextProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import CoinContextProvider from "./context/CoinContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CoinContextProvider>
+        <App />
+      </CoinContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
