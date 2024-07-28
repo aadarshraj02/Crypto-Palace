9d63237ec1f0ea2c0872659557a8fec7a8d4f9dc
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-28 10:57:20 +0530

    added switch cases for dropdown menu to set currency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,42 @@
+import { useContext } from "react";
+import { CoinContext } from "../context/CoinContext";
+
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  const currencyHandler = (e) => {
+    switch (e.target.value) {
+      case "usd": {
+        setCurrency({
+          name: "usd",
+          symbol: "$",
+        });
+        break;
+      }
+      case "eur": {
+        setCurrency({
+          name: "eur",
+          symbol: "€",
+        });
+        break;
+      }
+      case "inr": {
+        setCurrency({
+          name: "inr",
+          symbol: "₹",
+        });
+        break;
+      }
+      default: {
+        setCurrency({
+          name: "usd",
+          symbol: "$",
+        });
+        break;
+      }
+    }
+  };
+
   return (
     <div className="flex items-center justify-between py-4 px-10 border-['#3c3c3c'] border-b-2">
       <img src="/logo.png" alt="logo" />
@@ -9,10 +47,19 @@ const Navbar = () => {
         <li>Blog</li>
       </ul>
       <div className="flex items-center gap-5">
-        <select className="px-1 py-2 rounded-md bg-transparent">
-          <option className="text-black " value="usd">USD</option>
-          <option className="text-black " value="eur">EUR</option>
-          <option className="text-black " value="inr">INR</option>
+        <select
+          className="px-1 py-2 rounded-md bg-transparent"
+          onChange={currencyHandler}
+        >
+          <option className="text-black " value="usd">
+            USD
+          </option>
+          <option className="text-black " value="eur">
+            EUR
+          </option>
+          <option className="text-black " value="inr">
+            INR
+          </option>
         </select>
         <button className="bg-green-700 px-3 rounded-md cursor-pointer">
           Sign up
