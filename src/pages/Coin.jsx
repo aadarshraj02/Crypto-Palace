43e21c5c39996f18382be2d6a532396c76b06b49
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-28 19:56:24 +0530

    added async func to fetch data for specific coin id

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Coin = () => {
+  const { coinId } = useParams();
+  const [coinData, setCoinData] = useState();
+
+  const fetchCoinData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-a9PQo7NdvpLUsJm67GwVAnmg",
+      },
+    };
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
+
   return <div></div>;
 };
 
