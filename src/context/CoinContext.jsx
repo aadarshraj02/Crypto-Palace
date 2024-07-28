4c3de468f0dd1fa026d02990cdb312c7247c020d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-28 10:39:48 +0530

    added a async function to fetch coins data from api

--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -4,10 +4,26 @@ export const CoinContext = createContext();
 
 const CoinContextProvider = (props) => {
   const [allCoin, setAllCoin] = useState([]);
-  const [currency,setCurrency] =useState({
-    name:"usd",
-    symbol:"$"
+  const [currency, setCurrency] = useState({
+    name: "usd",
+    symbol: "$",
   });
+
+  const fetchAllCoins = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-a9PQo7NdvpLUsJm67GwVAnmg",
+      },
+    };
+
+    fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd", options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
+
   const contextValue = {};
 
   return (
