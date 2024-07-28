4ab4217f4da81e09d2723df15c71dc7b862abdb9
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-28 12:15:28 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Coin from "./pages/Coin";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/coin/:coinId" element={<Coin />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
