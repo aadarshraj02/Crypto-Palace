86f30dda872191acf5abf964d44e6112c1aecf2d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-28 20:47:32 +0530

    updated url for daily interval

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -34,7 +34,7 @@ const Coin = () => {
     };
 
     fetch(
-      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10`,
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`,
       options
     )
       .then((response) => response.json())
