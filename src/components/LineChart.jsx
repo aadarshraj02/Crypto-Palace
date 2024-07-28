0f2741eb04aa2f14125f983198927aff5c980a60
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-28 20:30:21 +0530

    added datat copy

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Chart from "react-google-charts";
 
-const LineChart = () => {
+const LineChart = ({ historicalData }) => {
   const [data, setData] = useState([["Date,Prices"]]);
+
+  useEffect(() => {
+    let dataCopy = [["Date", "Prices"]];
+    if(historicalData.prices){
+        historicalData.prices.map((item)=>{
+            dataCopy.push([`${new Date(item[0]).toLocaleDateString().slice(0,-5)}`,item[1]])
+        })
+        setData(dataCopy);
+    }
+  }, [historicalData]);
+
   return <div></div>;
 };
 
